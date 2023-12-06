982cd7ba7bc31d03cc7fcb2c6ec5627cf3833e37
yuhanz1 <yuhanz9149@gmail.com>
2023-12-06 10:14:07 -0500

    ????????

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,7 +1,13 @@
 import '../css/style.css';
 //npm run dev; npm run preview
 
-const URL = `https://random-d.uk/api/v2/quack`;
+//const URL = `https://random-d.uk/api/v2/quack`;
+const URL = `https://random.dog/woof.json`;
+//const URL = `http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]`;
+const response = await fetch(URL);
+console.log(response);
+
+/* 
 async function getData(URL){
     try{
         //whem u ask the computer for smth, you are requesting a response REST APIs; relational estfdjkg transfer;
@@ -12,10 +18,10 @@ async function getData(URL){
         }
         //convert response to JSON
         const data = await response.json();
-        document.querySelector("h2").textContent = data.content;
+        document.querySelector(".container").innerHTML = `<img src= ${URL.url}>`;
     } catch (error) {
         console.log(error, "uh oh");
         document.querySelector("h2").textContent = "something happened";
     }
 }
-getData(URL);
+getData(URL); */
