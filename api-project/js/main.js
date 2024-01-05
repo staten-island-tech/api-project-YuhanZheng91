eddf0ec81635c15caf874cd22d3697bb6db0220f
yuhanz1 <yuhanz9149@gmail.com>
2024-01-05 10:12:56 -0500

    final ? (2)

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -38,7 +38,7 @@ async function getData(){
             document.querySelector("h7").textContent = "";
         } catch (error) {
             console.log(error, "Uh oh! Something went wrong ...")
-            document.querySelector("h7").textContent = "Please enter a word.";
+            document.querySelector("h7").textContent = "Please enter a real word.";
         }
     })
 }
@@ -56,7 +56,7 @@ async function create(data) {
                 <div class="meanings">
                     <h4>${meaning.partOfSpeech}</h4>
                     <h5 class="def">Definition: ${meaning.definitions[0].definition}</h5>
-                    <h5>Synonym(s): ${meaning.synonyms}</h5>
+                    <h5>Synonym(s): ${meaning.synonyms} </h5>
                     <h5>Antonyms(s): ${meaning.antonyms}</h5>
                 </div>
                 <div class="sources">
@@ -68,29 +68,3 @@ async function create(data) {
         document.querySelector(".container").innerHTML += card;
     });
 }
-
-
-
-/*
-function create(data){
-    document.querySelector(".container").innerHTML = '';
-    const card = `
-    <div class="card">
-            <h3> ${data.word} </h3>
-        <div class="meanings">
-            <h4> ${data.partOfSpeech} </h4>
-            <h5 class="def"> Definition: ${data.definitions} </h5>
-            <h5> Synonym(s): ${data.synonyms} </h5>
-            <h5> Antonyms(s): ${data.antonyms} </h5>
-            <h5 class = "ex-sentence"> Sentence: ${data.example} </h5>
-        </div>
-        <div class="sources">
-            <h6 class="s-head"> Source: "${data.sourceUrls}"  </h6>
-            <h6> License: ${data.license} && ${data.license}</h6>
-        </div>
-    </div>
-    `;
-    document.querySelector(".container").insertAdjacentHTML("afterbegin", card);
-    
-}
-*/
